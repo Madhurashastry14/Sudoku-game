8a30d4f6bd04043c448c7841256c633252290ad0
Madhura Shastry <madhurashastry01@gmail.com>
2026-05-20 22:05:31 +0530

    elements appears but still repeated

---
+++ sudoku.js
@@ -0,0 +1,68 @@
+let row = 9;
+let col = 9;
+let table = [];
+
+for (let i = 0; i < row; i++) {
+  const currentrow = [];
+  for (let j = 0; j < col; j++) {
+    currentrow.push({
+      placed: false,
+      value: 0,
+    });
+  }
+  table.push(currentrow);
+}
+
+const box = document.getElementById("block");
+for (let i = 0; i < row; i++) {
+  for (let j = 0; j < col; j++) {
+    const cell = document.createElement("button");
+    cell.dataset.row = i;
+    cell.dataset.col = j;
+    cell.id = `${i}-${j}`;
+    cell.classList.add("cell");
+    if (j == 2 || j == 5) {
+      cell.classList.add("border-right");
+    }
+    if (i == 2 || i == 5) {
+      cell.classList.add("border-bottom");
+    }
+    box.appendChild(cell);
+  }
+}
+let countElements = Math.floor(Math.random() * 81);
+
+for (let i = 0; i < countElements; i++) {
+  let choice = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let numchoice = choice[Math.floor(Math.random() * choice.length)];
+  placeElements(numchoice);
+}
+
+function placeElements(numchoice) {
+  let r = Math.floor(Math.random() * row);
+  let c = Math.floor(Math.random() * col);
+  let cell = document.getElementById(`${r}-${c}`);
+
+  if (validPosition(r, c, numchoice)) {
+    table[r][c].placed = true;
+    table[r][c].value = numchoice;
+    cell.textContent = numchoice;
+  }
+}
+
+function validPosition(r, c, numchoice) {
+  let val = true;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (i == r && table[i][j].value == numchoice) {
+        val = false;
+      }
+      if (j == c && table[i][j].value == numchoice) {
+        val = false;
+      }
+    }
+  }
+  return val;
+}
+
+// document.getElementById("box").addEventListener("click", function (event) {});
