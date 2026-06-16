54c267a6c86578ee497cf2cd20264f0e7ed25606
Madhura Shastry <madhurashastry01@gmail.com>
2026-06-16 15:34:35 +0530

    add loading animation

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let play = document.getElementById("play");
+let loading = document.getElementById("loading");
+let text = document.getElementById("text");
+let container = document.getElementById("container");
+
 play.addEventListener("click", function () {
-  window.location.href = "../sudoku/sudoku.html";
+  loading.style.display = "flex";
+  text.style.display = "none";
+  play.style.display = "none";
+  setTimeout(() => {
+    window.location.href = "../sudoku/sudoku.html";
+  }, 1000);
 });
